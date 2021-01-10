d302afa9a8da971a6a64ad4812630f57c90b6e3c
diligence-dev <diligence.dev@web.de>
2021-01-10 15:12:47 +0100

    initial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,28 @@
 import React from "react"
+import { useQueryParam, NumberParam } from "use-query-params"
+import { shuffle } from 'shuffle-seed'
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const UseQueryParamExample = () => {
+  // something like: ?x=123&foo=bar in the URL
+  const [seed, setSeed] = useQueryParam("seed", NumberParam)
+  const [nPlayers, setNPlayers] = useQueryParam("nplayers", NumberParam)
+  const [position, setPosition] = useQueryParam("position", NumberParam)
+
+  return (
+    <div>
+      {shuffle({
+        3: ['Seherin', 'Werwolf', 'Dorfi'],
+        4: ['Seherin', 'Werwolf', 'Dorfi', 'Dorfi'],
+        5: ['Seherin', 'Werwolf', 'Dorfi', 'Dorfi', 'Dorfi'],
+        6: ['Seherin', 'Werwolf', 'Dorfi', 'Dorfi', 'Dorfi', 'Dorfi'],
+        7: ['Seherin', 'Werwolf', 'Dorfi', 'Dorfi', 'Dorfi', 'Dorfi', 'Werwolf'],
+        8: ['Seherin', 'Werwolf', 'Dorfi', 'Dorfi', 'Dorfi', 'Dorfi', 'Werwolf', 'Dorfi'],
+        9: ['Seherin', 'Werwolf', 'Dorfi', 'Dorfi', 'Dorfi', 'Dorfi', 'Werwolf', 'Freimaurer', 'Freimaurer'],
+        10: ['Seherin', 'Werwolf', 'Dorfi', 'Dorfi', 'Dorfi', 'Dorfi', 'Werwolf', 'Freimaurer', 'Freimaurer', 'Dorfi']
+        }[nPlayers], seed)[position]
+      }
+    </div>
+  );
+};
+
+export default UseQueryParamExample
